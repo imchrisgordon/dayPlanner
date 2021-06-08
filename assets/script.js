aed1896d0eac803a42b01a8049f8ded228344039
CGORE <imchrisgordon@gmail.com>
2021-06-08 15:49:24 -0700

    Created a better read me, will update with link to finished app

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
-moment().format('MMMM Do YYYY');
+moment().format('dddd MMMM Do YYYY');
+console.log(moment().format('dddd MMMM Do YYYY'))
 
-console.log(moment().format('MMMM Do YYYY'))
-
+// function to display todays day and date
 function headerDate() {
     var todaysDate = moment().format('dddd MMMM Do YYYY ');
-    $("#currentDay").text(todaysDate);
+    $('#currentDay').text(todaysDate);
 }
+//invoking function to display todays day and date
 headerDate();
-
-
+console.log(headerDate)
+//array of time slots
+timeSlots = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
+console.log(timeSlots)
---
+++ dayPlannerReadMe.md
@@ -0,0 +1,4 @@
+# dayPlanner
+A simple day planner app.
+
+This simple day planner app will allow you to add things to a to do list and then save them in the local storage. It will also color coordinate the time slots by past(gray) present(red) and future(green).
