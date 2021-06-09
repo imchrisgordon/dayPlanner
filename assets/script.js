1339b2bb38965832fa62435c2f09d2f3522b3dc6
CGORE <imchrisgordon@gmail.com>
2021-06-08 18:29:52 -0700

    JavaScript worked out and we should have a functioning day planner.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,49 @@
-moment().format('dddd MMMM Do YYYY');
-console.log(moment().format('dddd MMMM Do YYYY'))
-
-// function to display todays day and date
-function headerDate() {
-    var todaysDate = moment().format('dddd MMMM Do YYYY ');
-    $('#currentDay').text(todaysDate);
+const today = new Date();
+function getMarker(time) {
+  return (
+    "" + today.getFullYear() + (today.getMonth() + 1) + today.getDate() + time
+  );
 }
-//invoking function to display todays day and date
-headerDate();
-console.log(headerDate)
-//array of time slots
-timeSlots = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
-console.log(timeSlots)
+//Changes military time to standard time essentially
+function getFormattedHours(hours) {
+  if (hours >= 12) return hours == 12 ? "12 PM" : (hours % 12) + " PM";
+  else return hours + " AM";
+}
+//
+window.onload = function () {
+  document.querySelector("#currentDay").innerText = new moment().format(
+    "dddd MMMM Do YYYY"
+  );
+  const hours = [09, 10, 11, 12, 13, 14, 15, 16, 17];
+  let lineEntry = "";
+  hours.forEach(function (item) {
+    lineEntry +=
+      '<form class="row"><div class="col-md-2 hour">' +
+      getFormattedHours(item) +
+      '</div><div class="col-md-9 description p-0"><textarea id="text' +
+      item +
+      '" data-time="' +
+      item +
+      '"></textarea></div><button class="col-md-1 saveBtn" data-time="' +
+      item +
+      '" type="button"><i class="far fa-save fa-lg"></i></button></form>';
+  });
+  document.querySelector("#lineEntries").innerHTML = lineEntry;
+  document.querySelectorAll("textarea").forEach(function (item) {
+    let time = item.getAttribute("data-time"),
+      hours = today.getHours();
+    if (time == hours) item.classList.add("present");
+    if (time < hours) item.classList.add("past");
+    if (time > hours) item.classList.add("future");
+    item.value = localStorage.getItem(getMarker(time));
+  });
+  document.querySelectorAll("button").forEach(function (item) {
+    let time = item.getAttribute("data-time");
+    item.addEventListener("click", function () {
+      localStorage.setItem(
+        getMarker(time),
+        document.querySelector("#text" + time).value
+      );
+    });
+  });
+};
